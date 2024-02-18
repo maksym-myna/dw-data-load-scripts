b881fb6ee6273c41d29c9732b97eb785ba811c81
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-02-18 04:44:01 +0200

    [feature] abstract openlibrary data parser created

---
+++ scripts/parsers/ol_abstract_parser.py
@@ -0,0 +1,32 @@
+from abc import abstractmethod 
+from .abstract_parser import AbstractParser
+
+class OLAbstractParser(AbstractParser):
+    """
+    This is an abstract class for parsing Open Library files.
+    """
+
+    @abstractmethod
+    def process_latest_file(self, directory):
+        """
+        Process the latest file in the specified directory.
+
+        Parameters:
+        - directory (str): The directory path where the files are located.
+
+        Returns:
+        None
+        """
+        pass
+    
+    def parse_id(self, key):
+        """
+        Parse the ID from the given key.
+
+        Parameters:
+        - key (str): The key from which to extract the ID.
+
+        Returns:
+        str: The extracted ID.
+        """
+        return key.split('/')[-1]
